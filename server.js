263b88119d5406b06f03250b078524531d56cb76
Naman Mittal <mittalnaman2706@gmail.com>
2019-06-13 10:05:48 +0530

    Message Area displayed

--- server.js
+++ server.js
@@ -12,16 +12,30 @@ app.get('/', function(req, res){
 
 io.sockets.on('connection', function(socket){
 	connections.push(socket);
-	console.log("Connected: %s sockets connexted", connections.length);
+	console.log("Connected: %s sockets connected", connections.length);
 
 	socket.on('disconnect', function(data){
+		// if(!socket.username) return;
+		users.splice(users.indexOf(socket.username), 1);
+		updateUsernames();
 		connections.splice(connections.indexOf(socket), 1);
 		console.log("Disconnected: %s sockets connected", connections.length);		
 	});
 
 	socket.on('send message', function(data){
+		console.log(data);
 		io.sockets.emit('new message', {msg:data});
 	});
+	socket.on('new user', function(data, callback){
+		callback(true);
+		socket.username = data;
+		users.push(socket.username);
+		updateUsernames();
+	});
+
+	function updateUsernames(){
+		io.sockets.emit('get users', users);
+	}
 });
 
 server.listen(3000);
