863a61128b12f64764a98f5581a70fdb044f89ff
Naman Mittal <mittalnaman2706@gmail.com>
2019-07-10 20:50:45 +0530

    Backend Complete

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Web-Chat
+
+* RealtimeWeb-Chat application.
+* Built with Node.js and socket.io library.
+
+**Screenshot** 
+
+![Screenshot](./Screenshot.png)
--- server.js
+++ server.js
@@ -7,36 +7,42 @@ users = [];
 connections = [];
 
 app.get('/', function(req, res){
-	res.sendFile(__dirname + '/index.html');
+	res.sendFile(__dirname + '/main.html');
 });
 
 io.sockets.on('connection', function(socket){
 	connections.push(socket);
 	console.log("Connected: %s sockets connected", connections.length);
 
+	/* When a user disconnects from the chat */
 	socket.on('disconnect', function(data){
-		// if(!socket.username) return;
+
 		users.splice(users.indexOf(socket.username), 1);
 		updateUsernames();
 		connections.splice(connections.indexOf(socket), 1);
 		console.log("Disconnected: %s sockets connected", connections.length);		
 	});
 
+	/* Sending a message */
 	socket.on('send message', function(data){
 		console.log(data);
-		io.sockets.emit('new message', {msg:data});
+		console.log('username: ' + socket.username);
+		io.sockets.emit('new message', {msg:data, user: socket.username});
 	});
+
+	/* New User */
 	socket.on('new user', function(data, callback){
 		callback(true);
+		console.log(data);
 		socket.username = data;
-		users.push(socket.username);
+		users.push(data);
 		updateUsernames();
 	});
 
 	function updateUsernames(){
-		io.sockets.emit('get users', users);
+		io.sockets.emit('get_users', users);
 	}
 });
 
 server.listen(3000);
-console.log('Server Running on localhost:3000');
+console.log('Server Running on localhost:3000'); 
